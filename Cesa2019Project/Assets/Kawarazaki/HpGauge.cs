a36524318c601b0e0dabbf7fe6a2152c2682e629
tomo0704 <kss-17130014@c-league.jp>
2019-02-20 10:28:27 +0900

    タイマー、HPバー、星のUI

---
+++ Cesa2019Project/Assets/Kawarazaki/HpGauge.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// HPゲージ処理
+/// </summary>
+public class HpGauge : MonoBehaviour
+{
+    //緑ゲージのHPバー
+    [SerializeField]
+    public Image FrontHp;
+    //赤ゲージのHPバー
+    [SerializeField]
+    public Image BackHp;
+
+    //プレイヤーHP
+    [SerializeField]
+    float Hp = 0;
+    float MaxHp;
+
+    //プレイヤーが受けるダメージ
+    [SerializeField]
+    float Damege = 0;
+
+    //HPゲージ初期化
+    void Start()
+    {
+        MaxHp = Hp;
+        FrontHp = GameObject.Find("FrontHp").GetComponent<Image>();
+        FrontHp.fillAmount = 1.0f;
+        BackHp = GameObject.Find("BackHp").GetComponent<Image>();
+        BackHp.fillAmount = 1.0f;
+    }
+    
+
+    void Update()
+    {
+        //HP減少スピード変数
+        float reducespeed = 1.5f;
+        float waitspeed = 10.0f;
+
+        //ダメージ
+        if (Input.GetMouseButtonDown(0))
+        {
+            Hp -= Damege;
+            if (Hp < 0)
+                Hp = 0;
+        }
+        //回復
+        if (Input.GetMouseButtonDown(1))
+        {
+            Hp += Damege;
+            if (Hp > MaxHp)
+                Hp = MaxHp;
+            
+        }
+        //HPダメージ
+        FrontHp.fillAmount = Hp / MaxHp;
+
+        //FrontHpを追いかける
+        if (FrontHp.fillAmount <= BackHp.fillAmount)
+            BackHp.fillAmount -= reducespeed / waitspeed * Time.deltaTime;
+
+        //HP回復
+        if (FrontHp.fillAmount >= BackHp.fillAmount)
+            BackHp.fillAmount = FrontHp.fillAmount;
+
+
+        Debug.Log(Hp);
+    }
+}
---
+++ Cesa2019Project/Assets/Kawarazaki/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// カウントダウン処理
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    //制限時間(分)
+    [SerializeField]
+    private int Minute;
+    //制限時間(秒)
+    [SerializeField]
+    private float Seconds;
+    //トータル制限時間
+    private float TotalTime;
+    //前回Update時の秒数
+    private float OldSeconds;
+    private Text TimerText;
+
+    //タイマー初期化
+    void Start()
+    {
+        TotalTime = Minute * 60 + Seconds;
+        OldSeconds = 0.0f;
+        TimerText = GetComponentInChildren<Text>();
+    }
+    
+    void Update()
+    {
+        //制限時間が0秒以下なら何もしない
+        if(TotalTime <= 0.0f)
+        {
+            return;
+        }
+        //トータルの制限時間を計測
+        TotalTime = Minute * 60 + Seconds;
+        TotalTime -= Time.deltaTime;
+
+        //再設定
+        Minute = (int)TotalTime / 60;
+        Seconds = TotalTime - Minute * 60;
+
+        //タイマー表示用UIテキストに時間を表示する
+        if((int)Seconds != (int)OldSeconds)
+        {
+            TimerText.text = Minute.ToString("00") + ":" + ((int)Seconds).ToString("00");
+        }
+        OldSeconds = Seconds;
+
+        //制限時間以下になった時の処理
+        if(TotalTime <= 0.0f)
+        {
+            Debug.Log("制限時間終了");
+        }
+    }
+}
