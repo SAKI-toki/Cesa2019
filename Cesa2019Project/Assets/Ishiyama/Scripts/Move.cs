ba4bfac40cd87dfb2635e77adcd37badc2899a74
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-02-20 11:39:29 +0900

    カメラの追尾や回転の実装とその他プログラマ向けのテキストの追加

---
+++ Cesa2019Project/Assets/Ishiyama/Scripts/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    CameraController CameraCtrl;
+    [SerializeField]
+    Transform CameraTransform;
+    void Start()
+    {
+        CameraCtrl.CameraStart();
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 cameraForward = CameraTransform.forward;
+            cameraForward.y = 0;
+            pos += cameraForward.normalized * 10 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 cameraForward = CameraTransform.forward;
+            cameraForward.y = 0;
+            pos -= cameraForward.normalized * 10 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 cameraRight = CameraTransform.right;
+            cameraRight.y = 0;
+            pos += cameraRight.normalized * 10 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 cameraRight = CameraTransform.right;
+            cameraRight.y = 0;
+            pos -= cameraRight.normalized * 10 * Time.deltaTime;
+        }
+        transform.position = pos;
+
+        CameraCtrl.CameraUpdate();
+    }
+}
---
+++ Cesa2019Project/Assets/Ishiyama/Scripts/Status.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// ステータス
+/// </summary>
+public class Status
+{
+    //体力
+    public float Hp { get; set; }
+    //攻撃力
+    public float Attack { get; set; }
+    //防御力
+    public float Defense { get; set; }
+    //速さ
+    public float Speed { get; set; }
+    //現在の体力
+    public float CurrentHp { get; set; }
+    //現在の攻撃力
+    public float CurrentAttack { get; set; }
+    //現在の防御力
+    public float CurrentDefense { get; set; }
+    //現在の速さ
+    public float CurrentSpeed { get; set; }
+
+    /// <summary>
+    /// ステータスをリセットする
+    /// </summary>
+    public void ResetStatus()
+    {
+        CurrentHp = Hp;
+        CurrentAttack = Attack;
+        CurrentDefense = Defense;
+        CurrentSpeed = Speed;
+    }
+
+    /// <summary>
+    /// ダメージ
+    /// </summary>
+    /// <param name="attack">与える攻撃力</param>
+    /// <param name="defense">受ける防御力</param>
+    /// <returns></returns>
+    static public float Damage(float attack,float defense)
+    {
+        //TODO:ダメージ計算
+        float damage = attack - defense;
+        return (damage > 0) ? damage : 1;
+    }
+}
