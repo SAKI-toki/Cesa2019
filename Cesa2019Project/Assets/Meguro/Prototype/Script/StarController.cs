b85a584de50810681aee0b148f7da37102160aff
hide123 <kss-17130019@c-league.jp>
2019-02-20 12:29:23 +0900

    プリプロ版 プレイヤー操作（移動・アニメーション・星の取得・攻撃）

---
+++ Cesa2019Project/Assets/Meguro/Prototype/Script/StarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 星の動き
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class StarController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player = null;
+
+    Rigidbody Rigid;
+
+    Vector3 PlayerPos = Vector3.zero;
+    [SerializeField]
+    float Dis = 0;
+    [SerializeField]
+    float Speed = 0;
+
+    void Awake()
+    {
+        Rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        PlayerPos = Player.transform.position;
+        float distance = Vector3.Distance(transform.position, PlayerPos);
+
+        if(distance < Dis)
+        {
+            Vector3 direction = (PlayerPos - transform.position).normalized;
+
+            Rigid.AddForce(direction * Speed);
+        }
+    }
+}
---
+++ Cesa2019Project/Assets/Meguro/Prototype/Script/ゴミ箱/PlayerIdle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdle : MonoBehaviour,IPlayerMotion
+{
+    int count = 0;
+
+    public void Start()
+    {
+        Debug.Log("IdleStart");
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            ++count;
+            Debug.Log("Attack");
+        }
+        if(count > 2)
+        {
+            End();
+        }
+    }
+
+    public void End()
+    {
+        Next();
+    }
+
+    public void Next()
+    {
+
+    }
+}
