b94ae12d55aed44e8e72f98db23331a2e4bb7e1d
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-02-20 17:02:06 +0900

    リムライティングの作成

---
+++ Cesa2019Project/Assets/Ishiyama/Scripts/DebugCameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCameraController : MonoBehaviour
+{
+    [SerializeField]
+    float MoveSpeed;
+
+    float RotX = 0, RotY = 0;
+    void Update()
+    {
+        var pos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos -= transform.forward * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos += transform.right * MoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos -= transform.right * MoveSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+        float moveX, moveY;
+        moveX = Input.GetAxis("Mouse X");
+        moveY = Input.GetAxis("Mouse Y");
+        RotX -= moveY * Time.deltaTime * 10;
+        RotY += moveX * Time.deltaTime * 10;
+        transform.eulerAngles = new Vector3(RotX, RotY, 0);
+    }
+}
