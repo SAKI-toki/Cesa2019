811d68deae8664b1621c2aba6418d8d74e2311b4
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-02-22 11:04:18 +0900

    Merge remote-tracking branch 'origin/meguro'
    # Conflicts:
    #	Cesa2019Project/Assets/Cinemachine/CinemachineExamples.unitypackage.meta
    #	Cesa2019Project/Assets/Cinemachine/Timeline/LegacyTimelineAPI.unitypackage.meta
    #	Cesa2019Project/Assets/Ishiyama/Materials.meta
    #	Cesa2019Project/Assets/Ishiyama/Scenes/IshiyamaScene.unity.meta
    #	Cesa2019Project/Assets/Meguro/Prototype/Scene/PrototypeScene.unity
    #	Cesa2019Project/Assets/Meguro/Prototype/Script/PauseManager.cs.meta
    #	Cesa2019Project/Assets/Meguro/Prototype/Script/StarPlace.cs.meta
    #	Cesa2019Project/Assets/Meguro/Prototype/Script/StarPlaceManager.cs.meta
    #	Cesa2019Project/Assets/Meguro/Prototype/Script/ゴミ箱.meta
    #	Cesa2019Project/Assets/Unity Recorder.meta
    #	Cesa2019Project/Assets/Unity Recorder/Documentation.meta
    #	Cesa2019Project/Assets/Unity Recorder/Documentation/UnityRecorderV1UserManual.pdf.meta
    #	Cesa2019Project/ProjectSettings/DynamicsManager.asset
    #	Cesa2019Project/ProjectSettings/TagManager.asset
    #	Cesa2019Project/Recordings/movie.mp4

---
+++ Cesa2019Project/Assets/Meguro/Prototype/Script/StarPlace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 星を置く場所の情報を保持する
+/// </summary>
+public class StarPlace : MonoBehaviour
+{
+    [System.NonSerialized]
+    public Vector3 Pos = Vector3.zero;      // 位置
+    [System.NonSerialized]
+    public bool isActive = false;           // 星を置けるようになるフラグ
+    [System.NonSerialized]
+    public bool isSet = false;              // 星をセットしたフラグ
+
+    void Awake()
+    {
+        Pos = transform.position;
+        isActive = false;
+        isSet = false;
+    }
+}
