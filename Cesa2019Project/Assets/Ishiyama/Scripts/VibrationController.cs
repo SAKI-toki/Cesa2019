8cf54877a5ecc24fd22588568ec5db15c65b17f5
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-02-27 15:02:32 +0900

    バイブレーションのDLLを作成

---
+++ Cesa2019Project/Assets/Ishiyama/Scripts/VibrationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+public class VibrationController
+{
+    [DllImport("VibrationDll")]
+    static extern void Vibration(int n, float l_power, float r_power);
+    
+    static public void XVibration(int n, float l_power, float r_power)
+    {
+        Vibration(n, l_power, r_power);
+    }
+}
