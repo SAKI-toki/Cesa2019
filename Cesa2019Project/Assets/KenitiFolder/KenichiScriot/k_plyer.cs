982c853310a62607063856f42e6c5627d40b511e
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-02-27 15:14:23 +0900

    インデントをそろえる

--- Cesa2019Project/Assets/KenitiFolder/KenichiScriot/k_plyer.cs
+++ Cesa2019Project/Assets/KenitiFolder/KenichiScriot/k_plyer.cs
@@ -8,36 +8,36 @@ using UnityEngine;
 public class k_plyer : MonoBehaviour
 {
 
-    [SerializeField] float xmove=0;//X方向の移動力
-    [SerializeField] float zmove=0;//Z方向の移動力
+    [SerializeField] float xmove = 0;//X方向の移動力
+    [SerializeField] float zmove = 0;//Z方向の移動力
     // Start is called before the first frame update
     /// <summary>
     /// 
     /// </summary>
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow))//右に進む
-            transform.position += new Vector3(xmove*Time.deltaTime, 0, 0);
+            transform.position += new Vector3(xmove * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow))//左にすすむ
-            transform.position += new Vector3(-xmove*Time.deltaTime, 0, 0);
+            transform.position += new Vector3(-xmove * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.UpArrow))//上にすすむ
-            transform.position += new Vector3(0, 0, zmove*Time.deltaTime);
+            transform.position += new Vector3(0, 0, zmove * Time.deltaTime);
         if (Input.GetKey(KeyCode.DownArrow))//下に進む
-            transform.position += new Vector3(0, 0, -zmove*Time.deltaTime);
+            transform.position += new Vector3(0, 0, -zmove * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.A)) { transform.Rotate(0, -5, 0); }
         if (Input.GetKey(KeyCode.D)) { transform.Rotate(0, 5, 0); }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Time.timeScale != 0) { Time.timeScale = 0.3f;}
-            
+            if (Time.timeScale != 0) { Time.timeScale = 0.3f; }
+
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
