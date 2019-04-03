0c4a14ed4a18cf74cbfe6192f60d5a6001e58a9d
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-04-03 15:44:16 +0900

    エフェクトの作成

--- Cesa2019Project/Assets/Ishiyama/Scripts/Debugs/DebugRotation.cs
+++ Cesa2019Project/Assets/Ishiyama/Scripts/Debugs/DebugRotation.cs
@@ -10,6 +10,9 @@ public class DebugRotation : MonoBehaviour
     [SerializeField, Header("回転速度")]
     float RotationSpeed = 1.0f;
 
+    enum Axis { X, Y, Z };
+    [SerializeField, Header("軸")]
+    Axis RotationAxis = Axis.X;
     /// <summary>
     /// z軸の回転
     /// </summary>
@@ -18,12 +21,33 @@ public class DebugRotation : MonoBehaviour
         var rot = transform.eulerAngles;
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            rot.z += RotationSpeed * Time.deltaTime;
+            SwitchAxis(ref rot, RotationSpeed * Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.RightArrow))
         {
-            rot.z -= RotationSpeed * Time.deltaTime;
+            SwitchAxis(ref rot, -RotationSpeed * Time.deltaTime);
         }
         transform.eulerAngles = rot;
+    }
+
+    /// <summary>
+    /// 変数に応じて回転する軸を変更する
+    /// </summary>
+    /// <param name="rot">回転</param>
+    /// <param name="rotationValue">回転量</param>
+    void SwitchAxis(ref Vector3 rot, float rotationValue)
+    {
+        switch(RotationAxis)
+        {
+            case Axis.X:
+                rot.x += rotationValue;
+                break;
+            case Axis.Y:
+                rot.y += rotationValue;
+                break;
+            case Axis.Z:
+                rot.z += rotationValue;
+                break;
+        }
     }
 }
