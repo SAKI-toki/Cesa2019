ec8d4f6bb114fa5e3745e6dadb5d5dc8cbe1648e
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-04-12 12:09:30 +0900

    DebugHalo.csとDebugHaloEnemy.csにコメントを追加

--- Cesa2019Project/Assets/Ishiyama/Scripts/Debugs/DebugHaloEnemy.cs
+++ Cesa2019Project/Assets/Ishiyama/Scripts/Debugs/DebugHaloEnemy.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// 光輪を出す敵のサンプル
+/// </summary>
 public class DebugHaloEnemy : MonoBehaviour
 {
     [SerializeField, Header("ターゲット")]
@@ -8,20 +11,27 @@ public class DebugHaloEnemy : MonoBehaviour
     GameObject HaloObject = null;
     void Update()
     {
+        //エンターを押したら光輪を出す
         if (Input.GetKeyDown(KeyCode.Return))
         {
             ShotHalo();
         }
     }
 
+    /// <summary>
+    /// 光輪を出す
+    /// </summary>
     void ShotHalo()
     {
-        GameObject haloObjectRight = Instantiate(HaloObject, transform.position, Quaternion.identity);
-        var haloRight = haloObjectRight.GetComponent<DebugHalo>();
-        haloRight.HaloInit(TargetTransform.position, true);
-
-        GameObject haloObjectLeft = Instantiate(HaloObject, transform.position, Quaternion.identity);
-        var haloLeft = haloObjectLeft.GetComponent<DebugHalo>();
-        haloLeft.HaloInit(TargetTransform.position, false);
+        //右に出ていく光輪の生成
+        {
+            GameObject haloObjectRight = Instantiate(HaloObject, transform.position, Quaternion.identity);
+            haloObjectRight.GetComponent<DebugHalo>().HaloInit(TargetTransform.position, true);
+        }
+        //左に出ていく光輪の生成
+        {
+            GameObject haloObjectLeft = Instantiate(HaloObject, transform.position, Quaternion.identity);
+            haloObjectRight.GetComponent<DebugHalo>().HaloInit(TargetTransform.position, false);
+        }
     }
 }
