6aa14db694f854deb8d1bb2d41d66c7648d9c8ec
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-04-12 10:29:06 +0900

    星座作成

---
+++ Cesa2019Project/Assets/Ishiyama/Scripts/Debugs/DebugHaloEnemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DebugHaloEnemy : MonoBehaviour
+{
+    [SerializeField, Header("ターゲット")]
+    Transform TargetTransform = null;
+    [SerializeField, Header("光輪")]
+    GameObject HaloObject = null;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ShotHalo();
+        }
+    }
+
+    void ShotHalo()
+    {
+        GameObject haloObjectRight = Instantiate(HaloObject, transform.position, Quaternion.identity);
+        var haloRight = haloObjectRight.GetComponent<DebugHalo>();
+        haloRight.HaloInit(TargetTransform.position, true);
+
+        GameObject haloObjectLeft = Instantiate(HaloObject, transform.position, Quaternion.identity);
+        var haloLeft = haloObjectLeft.GetComponent<DebugHalo>();
+        haloLeft.HaloInit(TargetTransform.position, false);
+    }
+}
