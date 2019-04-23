220a3270eeb89ef3b762b56dcf9d9e940f7b7942
sisell <sisees7@gmail.com>
2019-04-23 16:36:41 +0900

    天秤座のボス修正

---
+++ Cesa2019Project/Assets/KenitiFolder/KenichiScriot/EnemyScript/OphiuchusEnemt.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OphiuchusEnemt : MonoBehaviour
+{
+    [SerializeField]
+    Enemy GetEnemy = null;
+    [SerializeField, Header("BOSSを入れる")]
+    GameObject[] Waves = null;// Waveプレハブを格納する
+
+    bool WaveStop = false;
+    int CurrentWave = 0;
+    GameObject Wave = null;
+    GameObject Wave2 = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetEnemy.NoDamage = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!WaveStop)
+        {
+            WaveGenerat();
+        }
+
+        if(Wave.transform.childCount==0)
+        {
+            GetEnemy.EnemyStatus.CurrentHp -= GetEnemy.EnemyHp / 6;
+            WaveStop = false;
+        }
+    }
+
+    /// <summary>
+    /// Waveの生成
+    /// </summary>
+    void WaveGenerat()
+    {
+        
+        // Waveを作成する
+        Wave = (GameObject)Instantiate(Waves[CurrentWave], transform.position, Quaternion.identity);
+        CurrentWave++;
+        Wave2 = (GameObject)Instantiate(Waves[CurrentWave], transform.position, Quaternion.identity);
+        CurrentWave++;
+        WaveStop = true;
+    }
+}
