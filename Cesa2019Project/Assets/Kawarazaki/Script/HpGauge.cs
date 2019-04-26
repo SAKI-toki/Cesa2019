d277de6e9e0e5e87078614b7f94c93d0a312f79d
tomo0704 <kss-17130014@c-league.jp>
2019-04-26 12:26:27 +0900

    HP減少時の画像変更

--- Cesa2019Project/Assets/Kawarazaki/Script/HpGauge.cs
+++ Cesa2019Project/Assets/Kawarazaki/Script/HpGauge.cs
@@ -7,38 +7,46 @@ using UnityEngine.UI;
 /// </summary>
 public class HpGauge : MonoBehaviour
 {
-    //HPバー(円)
-    [SerializeField]
-    public Image CircleHp = null;
-
     //HPバー(横)
     [SerializeField]
     public Image HorizontalHp = null;
 
-    //プレイヤーHP
-    //[SerializeField]
-    //float Hp = 200;
-    //float MaxHp = 250;
+    [SerializeField]
+    public Sprite Green;
+    [SerializeField]
+    public Sprite Yellow;
+    [SerializeField]
+    public Sprite Red;
+    //bool circleflg = false;
+    //bool horizonflg = true;
 
-    //プレイヤーが受けるダメージ
-    //[SerializeField]
-    //float Damage = 10;
-
-    bool circleflg = false;
-    bool horizonflg = true;
 
     //HPゲージ初期化
     void Start()
     {
-        //MaxHp = Hp;
-        CircleHp.fillAmount = 1.0f;
-        HorizontalHp.fillAmount= 1.0f;
+        HorizontalHp.fillAmount = 1.0f;
+        HorizontalHp.sprite = Green;
     }
 
 
     void Update()
     {
         HorizontalHp.fillAmount = PlayerController.PlayerStatus.CurrentHp / PlayerController.PlayerStatus.Hp;
+
+        if (HorizontalHp.fillAmount <= 0.3f)
+        {
+            HorizontalHp.sprite = Red;
+        }
+        else if (HorizontalHp.fillAmount <= 0.6f)
+        {
+            HorizontalHp.sprite = Yellow;
+        }
+        else
+        {
+            HorizontalHp.sprite = Green;
+        }
+
+
         ////ダメージ
         //if (Input.GetMouseButtonDown(0))
         //{
